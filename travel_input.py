bcb2e25bf7020e2f0a965b19231c5d3d6a8dc1c3
tammytdo11 <tammytdo11@gmail.com>
2023-09-30 16:45:53 -0700

    added weather bit api data

--- travel_input.py
+++ travel_input.py
@@ -7,12 +7,14 @@ import json
 
 openai.api_key = config.OPEN_AI_API_KEY
 google_places_api_key = config.GOOGLE_PLACES_API_KEY
+weatherbit_api_key = config.WEATHER_BIT_API_KEY
 
+# CREDIT to Sentdex
 # user_destination = input("Enter a city: ")
 user_destination = 'Hanoi'
 model_engine = "gpt-3.5-turbo"
 user_search_string = f"What is the typical weather in {user_destination} in the month of January?"
-'''
+
 completion = openai.ChatCompletion.create(
   model=model_engine,
   messages=[{"role": "user", "content": user_search_string}],
@@ -39,7 +41,7 @@ completion = openai.ChatCompletion.create(
 
 chatgpt_response = completion.choices[0].message.content
 print("chatgpt_response 2 >> ", chatgpt_response)
-'''
+
 
 #get place id
 google_place_id_url=f'https://maps.googleapis.com/maps/api/geocode/json?address={user_destination}&key={google_places_api_key}'
@@ -61,3 +63,48 @@ place_url = retreived_place_details["url"]
 print('place_full_name >>', place_full_name)
 print('place_icon >>', place_icon)
 print('place_url >>', place_url)
+
+
+# weather testing
+place_full_name='hanoi, vn'
+
+
+weather_data_list = [
+    {
+        'app_max_temp': 41.3, 'app_min_temp': 25.9, 'clouds': 39, 'clouds_hi': 8, 'clouds_low': 17,
+        'clouds_mid': 4, 'datetime': '2023-10-01', 'dewpt': 24.5, 'high_temp': 34.3, 'low_temp': 26,
+        'max_dhi': None, 'max_temp': 34.3, 'min_temp': 24.9, 'moon_phase': 0.964344, 'moon_phase_lunation': 0.54,
+        'moonrise_ts': 1696160000, 'moonset_ts': 1696120080, 'ozone': 266.8, 'pop': 0, 'precip': 0, 'pres': 1007.7,
+        'rh': 77, 'slp': 1009.2, 'snow': 0, 'snow_depth': 0, 'sunrise_ts': 1696114229, 'sunset_ts': 1696157333,
+        'temp': 29.3, 'ts': 1696093260, 'uv': 7.5, 'valid_date': '2023-10-01',
+        'weather': {'code': 802, 'icon': 'c02d', 'description': 'Scattered clouds'},
+        'wind_cdir': 'SSW', 'wind_cdir_full': 'south-southwest', 'wind_dir': 204, 'wind_gust_spd': 1.6, 'wind_spd': 1.4
+    }
+]
+
+
+#get weather data
+weatherbit_forecast_url = f'https://api.weatherbit.io/v2.0/forecast/daily?city={place_full_name}&key={weatherbit_api_key}'
+# print("weatherbit_forecast_url>> ", weatherbit_forecast_url)
+
+response_weatherbit_forecast = requests.get(weatherbit_forecast_url)
+converted_weather_response = json.loads(response_weatherbit_forecast.text)
+retreived_weather=converted_weather_response["data"][:7]
+
+# CREDIT Chatgpt
+class WeatherDay:
+  def __init__(self, weather_object):
+    self.date = weather_object.get('datetime', None)
+    self.min_temp = weather_object.get('min_temp' , None)
+    self.max_temp = weather_object.get('max_temp', None)
+    self.description = weather_object.get('weather', None).get('description', None)
+
+  def __str__(self):
+    return f"Date: {self.date}. Degrees: {self.min_temp}-{self.max_temp}. Description: {self.description}"
+
+weather_instances_list = [WeatherDay(weather_obj) for weather_obj in retreived_weather]
+
+for day in weather_instances_list:
+  print(day)
+
+
